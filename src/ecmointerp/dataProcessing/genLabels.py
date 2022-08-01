6714cdc47c2a77bb597b3fd6107ca5587a5ecf55
Isaac Sears <isaac.j.sears@gmail.com>
2022-08-01 11:43:41 -0400

    Generate labels for thrombosis and hemorrhage

--- src/ecmointerp/dataProcessing/genLabels.py
+++ src/ecmointerp/dataProcessing/genLabels.py
@@ -1,21 +1,106 @@
 import pandas as pd
 import os
 
-if __name__ == "__main__":
-    stay_ids = [int(dirname) for dirname in os.listdir("cache/mimicts")]
 
-    labels = pd.DataFrame(index=stay_ids, data={"label": [0] * len(stay_ids)})
+def _handle_labels(hadm_group, label_codes):
+    ret = pd.Series(index=label_codes.keys(), dtype="float")
+    for outcome, relevant_codes in label_codes.items():
+        ret.loc[outcome] = hadm_group["icd_code"].isin(relevant_codes).any()
 
-    def get_sepsis(stay_id: int):
-        df = pd.read_csv(f"cache/mimicts/{stay_id}/sepsis3_features.csv")
-        # If there's an entry in the dataframe, there must be sepsis
-        # Super simplistic for now
-        return int(len(df) != 0)
+    return ret
 
-    labels["label"] = labels.index.map(get_sepsis)
 
-    print(
-        f"Generated labels, (+) label prevalance: {labels['label'].sum() / len(labels):.4f}"
+def do_agg(stay_ids):
+    label_codes = dict()
+    icustays = pd.read_csv("mimiciv/icu/icustays.csv")
+
+    icustays = icustays[icustays["stay_id"].isin(stay_ids)]
+
+    # Labels
+    diagnoses = pd.read_csv("mimiciv/hosp/diagnoses_icd.csv")
+    diagnoses = diagnoses[diagnoses["hadm_id"].isin(icustays["hadm_id"])]
+
+    # Thrombosis
+    dvt_codes = (
+        diagnoses[diagnoses["icd_code"].str.startswith("I8240")]["icd_code"]
+        .unique()
+        .tolist()
+    )
+    dvt_codes += (
+        diagnoses[diagnoses["icd_code"].str.startswith("4534")]["icd_code"]
+        .unique()
+        .tolist()
     )
 
-    labels.to_csv("cache/labels.csv")
+    pe_codes = (
+        diagnoses[diagnoses["icd_code"].str.startswith("I26")]["icd_code"]
+        .unique()
+        .tolist()
+    )
+    pe_codes += (
+        diagnoses[diagnoses["icd_code"].str.startswith("4151")]["icd_code"]
+        .unique()
+        .tolist()
+    )
+    oxygenator_thrombosis_codes = (
+        diagnoses[diagnoses["icd_code"].isin(["T82867A", "99672"])]["icd_code"]
+        .unique()
+        .tolist()
+    )
+    unspecified_thrombosis_codes = (
+        diagnoses[
+            (diagnoses["icd_code"] == "4449")
+            | (diagnoses["icd_code"].str.startswith("I74"))
+        ]["icd_code"]
+        .unique()
+        .tolist()
+    )
+
+    label_codes["Thrombosis"] = (
+        dvt_codes
+        + pe_codes
+        + oxygenator_thrombosis_codes
+        + unspecified_thrombosis_codes
+    )
+
+    # Hemorrhage
+    ic_hemorrhage_codes = (
+        diagnoses[diagnoses["icd_code"] == "431"]["icd_code"].unique().tolist()
+    )
+    ic_hemorrhage_codes += (
+        diagnoses[diagnoses["icd_code"].str.startswith("I61")]["icd_code"]
+        .unique()
+        .tolist()
+    )
+    gi_hemorrhage_codes = ["578", "5780", "5781", "5789", "K922"]
+    csite_hemorrhage_codes = ["99674", "T82838A"]
+    ssite_hemorrhage_codes = ["L7622", "99811"]
+    pulm_hemorrhage_codes = ["R0489", "78639"]
+    epistaxis_codes = ["R040", "7847"]
+    nonspecific_bleeding_codes = ["99811", "I97418", "I9742", "I97618", "I9762"]
+    label_codes["Hemorrhage"] = (
+        ic_hemorrhage_codes
+        + gi_hemorrhage_codes
+        + csite_hemorrhage_codes
+        + ssite_hemorrhage_codes
+        + pulm_hemorrhage_codes
+        + epistaxis_codes
+        + nonspecific_bleeding_codes
+    )
+
+    labels_df = diagnoses.groupby("hadm_id").apply(
+        _handle_labels, label_codes=label_codes
+    )
+    labels_df = icustays.merge(
+        labels_df, how="left", left_on="hadm_id", right_index=True
+    )
+
+    return labels_df[["stay_id"] + list(label_codes.keys())]
+
+
+if __name__ == "__main__":
+    stay_ids = pd.read_csv("cache/included_stayids.csv").squeeze("columns").to_list()
+
+    labels_df = do_agg(stay_ids)
+
+    labels_df.to_csv("cache/labels.csv", index=False)
